7e56a02262c44a13addd9104436f897efb80021a
Alexis Bonhomme <bonhommealexis@gmail.com>
2025-05-07 12:38:06 +0000

    rezfezkn

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,14 +1,16 @@
 SELECT 
     date_date,
     count(orders_id) AS nb_transactions,
-    sum(order_revenue) AS revenue,
-    sum(order_revenue)/count(orders_id) AS average_basket, 
-    sum(order_margin) AS margin,
-    sum(oper_margin) AS operating_margin,
-    sum(order_purchase) AS total_purchase_cost,
-    sum(shipping_fee) AS total_shipping_fees,
-    sum(logcost) AS total_log_costs,
-    sum(quantity) AS quantity_product_sold
+    ROUND(sum(order_revenue),2) AS revenue,
+    ROUND(sum(order_revenue)/count(orders_id),2) AS average_basket, 
+    ROUND(sum(order_margin),2) AS margin,
+    ROUND(sum(oper_margin),2) AS operating_margin,
+    ROUND(sum(order_purchase),2) AS total_purchase_cost,
+    ROUND(sum(shipping_fee),2) AS total_shipping_fees,
+    ROUND(sum(logcost),2) AS total_log_costs,
+    ROUND(sum(order_quantity),2) AS quantity_product_sold
 FROM {{ ref('int_orders_operational') }}
 GROUP BY 
     date_date
+ORDER BY   
+    date_date DESC
