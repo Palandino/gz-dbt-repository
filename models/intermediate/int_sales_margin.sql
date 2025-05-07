b21b519145a5cf3ef4e6551749a3e92eedbb189b
Alexis Bonhomme <bonhommealexis@gmail.com>
2025-05-07 13:23:08 +0000

    iuu

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,11 +1,11 @@
 WITH margin AS (
 select
-    date_date,
-    orders_id,
-    product_id,
-    revenue,
-    quantity,
-    purchase_price
+    sales.date_date,
+    sales.orders_id,
+    sales.product_id,
+    sales.revenue,
+    sales.quantity,
+    purchase.purchase_price
 from {{ ref('stg_gz_raw_data__gz_sales') }} AS sales
     LEFT JOIN {{ ref('stg_gz_raw_data__gz_product') }} AS purchase
 on sales.product_id = purchase.products_id
